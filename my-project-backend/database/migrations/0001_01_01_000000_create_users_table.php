22faa344ca205e42b65149df551644674b40d0f1
Tommy76543 <trtrthai1999@gmail.com>
2025-09-10 13:31:54 +0700

    email_verified_at

--- my-project-backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ my-project-backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id('user_id');
             $table->string('name', 100);
             $table->string('email', 100)->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('phone', 15)->nullable();
             $table->string('address', 255)->nullable();
             $table->enum('gender', ['male', 'female', 'other'])->nullable();
