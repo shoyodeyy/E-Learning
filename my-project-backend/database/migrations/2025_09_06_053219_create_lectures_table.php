4a169d7edd2acb9cf30e8528c9e28d7b894398df
MTH <chunhau.py@gmail.com>
2025-09-10 09:20:29 +0700

    add field videoName

--- my-project-backend/database/migrations/2025_09_06_053219_create_lectures_table.php
+++ my-project-backend/database/migrations/2025_09_06_053219_create_lectures_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->integer('lectureIndex');
             $table->string('videoUrl')->nullable();
             $table->string('videoFile')->nullable();
-            $table->string('videoName')->nullable();
             $table->string('thumbnail')->nullable();
             $table->integer('lectureDuration')->nullable();
 
